2e9c12f77059b175178fa88303659ed1eb8b3e77
bauer Jack <gaius.july@gmail.com>
2022-02-20 20:29:34 +0900

    .

--- src/pages/dashboard/AboutViewPage.tsx
+++ src/pages/dashboard/AboutViewPage.tsx
@@ -10,44 +10,24 @@ export default class AboutViewPage extends React.Component<{}, {}> {
     return (
       <div>
         <Container text style={{ marginTop: '7em' }}>
-          {/* <Header as='h2'>To About</Header>
-            <p>
-            My name is 오세영 and I am an experienced software engineer and developer <br/>
-            I have a Bachelor's degree in Computer Science and several years of experience working on a wide range of projects <br/>
-            on government, manufacture, ect <br/>
-            only focusing on web app It is the main chief element of my business success.<br/>
-            </p>
-            <p>
-            Military service has been a artillery.<br/>
-            Got a data processing engineer certificated and registered in KOSA<br/>
-            </p>
-            <p>
-            I have been experienced with from lagacy to modern web app presently having been interested cBioPortal and build my resume as a Typescript React.
-            </p> */}
-
+        
      <Card fluid>
       <Card.Content>
-        {/* <Image style={{width:'70px',height:'90px'}}
-          floated='right'
-          size='mini'
-          src={require('../../rootImages/po1.jpg')}
-        /> */}
         <Card.Header>To About</Card.Header>
         {/* <Card.Meta>010-3487-2484 (gaius.july@gmail.com)</Card.Meta> */}
         <Card.Description>
         <p>
             My name is 오세영 and I am an experienced software engineer and developer <br/>
             I have a Bachelor's degree in Computer Science and several years of experience working on a wide range of projects <br/>
-            on government, manufacture, ect <br/>
-            only focusing on web app It is the main chief element of my business success.<br/>
+            only focused runned in web app It is the main chief element of my business success.<br/>
             </p>
             <p>
-            Military service has been a artillery.<br/>
-            Got a data processing engineer certificated and registered in KOSA<br/>
+            Military service finished in artillery.<br/>
+            Got the data processing engineer certificated and have been registered in KOSA<br/>
             </p>
             <p>
             I have been experienced with from lagacy to modern web app, presently having been interested cBioPortal and build my resume as a Typescript React.<br/>
-            Stand with Me.
+            it's a greate job.
             </p>
             <p>
             안녕하세요!반갑습니다<br />
